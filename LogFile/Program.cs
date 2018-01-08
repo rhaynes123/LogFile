4fc35747bd2c93a7f4024b66bccfcca9a5bdeb42
Richard <rahayn@yahoo.com>
2018-01-07 22:08:44 -0500

    Creating Textiles
    Create a textile

---
+++ LogFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace LogFile
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = new StreamReader(@"tests.txt");
+                streamReader.Close();
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                StreamWriter writeLogfile = new StreamWriter(@"test.txt");
+                //Console.WriteLine(ex.StackTrace);
+            }
+            if (streamReader != null)
+            {
+                streamReader.Close();
+
+            }
+        }
+    }
+}
